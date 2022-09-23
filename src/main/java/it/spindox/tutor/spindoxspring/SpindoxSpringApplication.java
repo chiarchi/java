de25b09e5ce66325f06ef9cc5113c20ec573ff54
alberto.massa <alberto.massa@spindox.it>
2022-09-23 11:29:41 +0200

    [master] init project

---
+++ src/main/java/it/spindox/tutor/spindoxspring/SpindoxSpringApplication.java
@@ -0,0 +1,13 @@
+package it.spindox.tutor.spindoxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpindoxSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpindoxSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/spindox/tutor/spindoxspring/SpindoxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.spindox.tutor.spindoxspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpindoxSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
